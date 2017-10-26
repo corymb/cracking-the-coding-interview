f2eaa822160370fdc9b91da8a17d062d5c14515a
Dave Bush <github@david-bush.co.uk>
2017-10-26 20:32:59 +1300

    Working insert method.

--- src/chapter_1/e1_hash_table.py
+++ src/chapter_1/e1_hash_table.py
@@ -1,5 +1,15 @@
 
 class HashTable:
-    def __len__(self):
-        return 0
+    def __init__(self):
+        self.buckets = [[] for x in range(11)]
 
+    def __len__(self):
+        return len([b for b in self.buckets if b])
+
+    def __repr__(self):
+        return '{}'.format([b for b in self.buckets if b])
+
+    def insert(self, key, value):
+        bucket = hash(key) % len(self.buckets)
+        self.buckets[bucket].append('{0}:{1}:{2}'.format(
+            hash(key), key, value))
--- src/chapter_1/tests/test_e1_hash_table.py
+++ src/chapter_1/tests/test_e1_hash_table.py
@@ -1,8 +1,14 @@
 from chapter_1.e1_hash_table import HashTable
 
-def test_hash_table():
-    assert HashTable()
 
 def test_empty_hash_table():
     hash_table = HashTable()
     assert len(hash_table) == 0
+
+
+def test_insert():
+    hash_table = HashTable()
+    hash_table.insert('test', 5)
+    assert len(hash_table) == 1
+    assert 'test' in hash_table.__repr__()
+    assert str(5) in hash_table.__repr__()
