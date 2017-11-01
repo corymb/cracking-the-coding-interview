e2705b58c3cfd29859ca5d235e04ce708ea4608a
Dave Bush <github@david-bush.co.uk>
2017-11-01 20:40:00 +1300

    Added retrieve method, tests and hooked into object model.

--- src/chapter_1/e1_hash_table.py
+++ src/chapter_1/e1_hash_table.py
@@ -9,7 +9,21 @@ class HashTable:
     def __repr__(self):
         return '{}'.format([b for b in self.buckets if b])
 
+    def __setitem__(self, key, value):
+        self.insert(key, value)
+
+    def __getitem__(self, key):
+        return self.retrieve(key)
+
     def insert(self, key, value):
         bucket = hash(key) % len(self.buckets)
         self.buckets[bucket].append('{0}:{1}:{2}'.format(
             hash(key), key, value))
+
+    def retrieve(self, key):
+        bucket = hash(key) % len(self.buckets)
+        for item in self.buckets[bucket]:
+            if item.startswith(str(hash(key))):
+                __, __, v = item.split(':')
+                return v
+        raise KeyError
--- src/chapter_1/tests/test_e1_hash_table.py
+++ src/chapter_1/tests/test_e1_hash_table.py
@@ -1,3 +1,5 @@
+import pytest
+
 from chapter_1.e1_hash_table import HashTable
 
 
@@ -13,3 +15,24 @@ def test_insert(single_key_value_pair):
     assert len(hash_table) == 1
     assert k in hash_table.__repr__()
     assert v in hash_table.__repr__()
+
+
+def test_retrieve(hash_table, single_key_value_pair):
+    k, v = single_key_value_pair
+    assert hash_table.retrieve(k) == v
+
+
+def test_object_model_set(single_key_value_pair):
+    hash_table = HashTable()
+    key, value = single_key_value_pair
+    hash_table[key] = value
+
+
+def test_object_model_get(hash_table, single_key_value_pair):
+    key, value = single_key_value_pair
+    assert hash_table[key] == value
+
+
+def test_key_error(hash_table):
+    with pytest.raises(KeyError):
+        hash_table['non_existent_key']
